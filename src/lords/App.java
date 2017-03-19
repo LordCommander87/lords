c8299cb3a2bff0215d56250b17908cbe5ab5442c
Mario <lordscorterrae@gmail.com>
2017-03-19 15:27:28 -0300

    Novo Monstro e implementações

--- src/lords/App.java
+++ src/lords/App.java
@@ -3,7 +3,19 @@ package lords;
 public class App {
 
 	public static void main(String[] args) {
-
+		Personagem olaf = new Personagem("Olaf",100,10,10,10);
+		Batalha westeros = new Batalha(olaf);
+		
+		westeros.setMonstros();
+		System.out.println(olaf.toString());
+		System.out.println(westeros.getMonstros().toString());
+		System.out.println(westeros.golpearMonstro(null, 0));
+		System.out.println(westeros.getMonstros().toString());
+		System.out.println(westeros.golpearMonstro(null, 0));
+		System.out.println(westeros.getMonstros().toString());
+		System.out.println(westeros.golpearPersonagem(null, 0));
+		System.out.println(westeros.getMonstros().toString());
+		System.out.println(olaf.toString());
 	}
 
 }
