d047d58d464f6561b3b9650fcd93a21e6a0927d6
Matheus Damasceno <matheusdamascenotx@gmail.com>
2017-03-19 13:52:10 -0300

    Base das classes Golpe,Inventario e Item

--- src/lords/Golpe.java
+++ src/lords/Golpe.java
@@ -1,5 +1,50 @@
 package lords;
 
 public class Golpe {
-
+	private String nome;
+	private int dano; 
+	private int stamina;
+	
+	public Golpe(String nome, int dano,int stamina) {
+		this.nome = nome;
+		this.dano = dano;
+		this.stamina = stamina;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getDano() {
+		return dano;
+	}
+	
+	public void setDano(int dano) {
+		this.dano = dano;
+	}
+	
+	public int getStamina() {
+		return stamina;
+	}
+	
+	public void setStamina(int stamina) {
+		this.stamina = stamina;
+	}
+	
+	public String toString(){
+		return "Golpe: Nome = " + nome + " " + "Dano = " + 
+				dano + " " + "Stamina = " + stamina + ".";
+ 	}
+	
+	public int calculaDano(Personagem p){
+		return dano = (((p.getForça()/100) * dano + dano) - p.getResistencia());
+	}
+	
+	public int calculaDano(Monstro m){
+		return dano = (((m.getForça()/100) * dano + dano) - m.getResistencia());
+	}
 }
--- src/lords/Item.java
+++ src/lords/Item.java
@@ -1,5 +1,73 @@
 package lords;
 
 public class Item {
-
+	private String nome;
+	private int vida;
+	private int força;
+	private int resistencia;
+	private int preço;
+	private int energia;
+	
+	public Item(String nome,int vida,int força,int resistencia,int preço,int energia){
+		this.nome = nome;
+		this.vida = vida;
+		this.força = força;
+		this.resistencia = resistencia;
+		this.preço = preço;
+		this.energia = energia;
+	}
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getVida() {
+		return vida;
+	}
+	
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
+	
+	public int getForça() {
+		return força;
+	}
+	
+	public void setForça(int força) {
+		this.força = força;
+	}
+	
+	public int getResistencia() {
+		return resistencia;
+	}
+	
+	public void setResistencia(int resistencia) {
+		this.resistencia = resistencia;
+	}
+	
+	public int getPreço() {
+		return preço;
+	}
+	
+	public void setPreço(int preço) {
+		this.preço = preço;
+	}
+	
+	public int getEnergia(){
+		return energia;
+	}
+	
+	public void setEnergia(int energia){
+		this.energia = energia;
+	}
+	
+	public String toString() {
+		return "Item [nome=" + nome + ", vida=" + vida + ", força=" + força + ", resistencia=" + resistencia
+				+ ", preço=" + preço + ", energia=" + energia + "]";
+	}
+	
+	
 }
